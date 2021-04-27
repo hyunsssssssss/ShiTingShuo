f5d4366ff8472f3c4acbcf664852532131a56d2d
Hyun <1286252638@qq.com>
2021-04-27 21:09:22 +0800

    fix version code

--- dist/main.meta.js
+++ dist/main.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        清华社视听说 - 自动答题
-// @version     0.4.0
+// @version     0.4.1
 // @author      Hyun
 // @description 解放你的双手
 // @include     *://www.tsinghuaelt.com/*
